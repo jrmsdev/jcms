0d59eb0ac45e20a6c7d675195b1cba62c534f1e5
Jeremías <jrmsdev@gmail.com>
2018-11-29 00:49:20 -0300

    CI: install golang on windows

--- check.py
+++ check.py
@@ -9,11 +9,17 @@ if '-v' in sys.argv:
 	verbose = " -v"
 
 prevcmd = {
-	10: "go install -i ./cmd/jcms",
+	10: "go version",
+	11: "go install -i ./cmd/jcms",
 	20: "go get -v -t ./...",
 	30: "go vet ./...",
 }
 gotest = "go test{} ./...".format(verbose)
+
+if sys.platform.startswith('win'):
+	prevcmd.update({
+		0: "choco install golang",
+	})
 
 for idx in sorted(prevcmd.keys()):
 	cmd = prevcmd[idx]
