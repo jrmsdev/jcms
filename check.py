37bc5420be53d2509b211c6f8aa87bc2eca79441
Jeremías <jrmsdev@gmail.com>
2018-11-29 02:32:07 -0300

    CI: try to fix window go exe

--- check.py
+++ check.py
@@ -8,19 +8,23 @@ verbose = ""
 if '-v' in sys.argv:
 	verbose = " -v"
 
-prevcmd = {
-	10: "go install -i ./cmd/jcms",
-	20: "go get -v -t ./...",
-	30: "go vet ./...",
-}
-gotest = "go test{} ./...".format(verbose)
-
+gocmd = "go"
+prevcmd = dict()
 if sys.platform.startswith('win'):
+	gocmd = "go.exe"
 	prevcmd.update({
 		0: "choco install golang",
 	})
 
-rc = call("go version".split())
+gotest = "{} test{} ./...".format(gocmd, verbose)
+prevcmd.update({
+	10: "{} install -i ./cmd/jcms".format(gocmd),
+	20: "{} get -v -t ./...".format(gocmd),
+	30: "{} vet ./...".format(gocmd),
+})
+
+
+rc = call([gocmd, "version"])
 if rc != 0:
 	sys.exit(rc)
 
