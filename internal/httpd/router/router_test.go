dc1ff49d1b455f10ab1379c96ca024d0e3632d5c
Jeremías <jrmsdev@gmail.com>
2018-12-01 16:10:31 -0300

    httpd router tests

--- internal/httpd/router/router_test.go
+++ internal/httpd/router/router_test.go
@@ -1,4 +1,21 @@
 // Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
 // See LICENSE file.
 
-package router
+package router_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/jrmsdev/jcms/internal/_t/check"
+	"github.com/jrmsdev/jcms/internal/httpd/router"
+)
+
+func TestRouterInit(t *testing.T) {
+	r := router.Init()
+	typ := fmt.Sprintf("%T", r)
+	if check.NotEqual(t, typ, "*mux.Router",
+		"invalid httpd router type") {
+		t.FailNow()
+	}
+}
