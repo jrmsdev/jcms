79379d1fa2a231f26c0bd95adefa77e1512dbeed
Jeremías <jrmsdev@gmail.com>
2018-12-01 15:28:26 -0300

    eval go env

--- distclean.sh
+++ distclean.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -eu
+eval `go env`
 DIR=${GOPATH}
+OSARCH=${GOOS}_${GOARCH}
 go clean -i ./..
 go clean -cache
 test -d ${DIR}/bin
 test -d ${DIR}/pkg
-rm -rfv ${DIR}/bin/jcms ${DIR}/pkg/*/github.com/jrmsdev/jcms*
+rm -rfv ${DIR}/bin/jcms ${DIR}/pkg/${OSARCH}/github.com/jrmsdev/jcms* | sort
