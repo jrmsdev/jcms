e51b0a0bbdaeb19cb458278c15cd11354c04bad4
Jeremías <jrmsdev@gmail.com>
2018-12-01 20:36:50 -0300

    assets tests

--- assets/assets_test.go
+++ assets/assets_test.go
@@ -1,4 +1,21 @@
 // Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
 // See LICENSE file.
 
-package assets
+package assets_test
+
+import (
+	"testing"
+
+	"github.com/jrmsdev/jcms/internal/_t/test"
+)
+
+func TestMain(m *testing.M) {
+	test.Main(m, "testing")
+}
+
+func TestManager(t *testing.T) {
+	c := test.Client(t)
+	r := c.Get("/static/test.txt")
+	r.Status(200)
+	r.Body("testing")
+}
