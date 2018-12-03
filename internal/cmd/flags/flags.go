f77e6d76d6424a1ed38c8c6577fe8dbed854bb4b
Jeremías <jrmsdev@gmail.com>
2018-12-03 18:32:43 -0300

    cmd flags webapp name

--- internal/cmd/flags/flags.go
+++ internal/cmd/flags/flags.go
@@ -9,12 +9,14 @@ var ShowVersion bool
 var Debug bool
 var Quiet bool
 var HttpPort int
+var Webapp string
 
 func init() {
 	flag.BoolVar(&ShowVersion, "V", false, "show version")
 	flag.BoolVar(&Debug, "d", false, "enable debug")
 	flag.BoolVar(&Quiet, "q", false, "quiet mode")
-	flag.IntVar(&HttpPort, "p", 0, "http `port` (default 0)")
+	flag.IntVar(&HttpPort, "p", 0, "http `port` (default \"0\")")
+	flag.StringVar(&Webapp, "n", "default", "`webapp` name")
 }
 
 func Parse() {
