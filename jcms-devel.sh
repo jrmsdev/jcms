50b5e07c3da5c3fff837683f39bca80140f7dfc2
Jeremías <jrmsdev@gmail.com>
2019-01-19 02:15:57 -0300

    split devel env

--- jcms-devel.sh
+++ jcms-devel.sh
@@ -1,13 +1,7 @@
 #!/bin/sh -eu
-srcdir=`pwd`
-mainsrc=./internal/_devel
-develcmd=./build/jcms-devel.bin
-basedir=./internal/_devel/assets
-datadir=./internal/_devel/data
+. ./jcms-devel.env
 go generate ./...
 mkdir -p build
 rm -f $develcmd
 go build -o $develcmd $mainsrc
-export JCMS_BASEDIR=$basedir
-export JCMS_DATADIR=$datadir
 $develcmd -D -n devel -p 6080
