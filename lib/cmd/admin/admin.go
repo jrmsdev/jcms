73c85c5d7beb4558bae64f49e9ae945bd7f9b455
Jeremías <jrmsdev@gmail.com>
2019-01-22 06:15:19 -0300

    use command line name

--- lib/cmd/admin/admin.go
+++ lib/cmd/admin/admin.go
@@ -15,13 +15,14 @@ import (
 )
 
 func Main() {
+	cmd := filepath.Base(os.Args[0])
 	flags.Parse()
 	if flags.ShowVersion {
-		fmt.Fprintf(os.Stderr, "jcms-admin version %s\n", jcms.Version())
+		fmt.Fprintf(os.Stderr, "%s version %s\n", cmd, jcms.Version())
 		os.Exit(0)
 	}
 	log.Init(flags.Log)
-	log.Printf("%s version %s", filepath.Base(os.Args[0]), jcms.Version())
+	log.Printf("%s version %s", cmd, jcms.Version())
 	log.Printf("http://127.0.0.1:%s/", flags.HttpPort)
 	srv := handler.Setup()
 	if err := srv.ListenAndServe(); err != nil {
